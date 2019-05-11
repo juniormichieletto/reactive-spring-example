58c888e5ab71bb95378d5183ce271694984dfefb
Junior Miqueletti <juniormiqueletti@gmail.com>
2019-05-11 10:25:57 -0300

    First commit

---
+++ src/main/java/com/spring/reactive/reactivespringexample/ReactiveSpringExampleApplication.java
@@ -0,0 +1,13 @@
+package com.spring.reactive.reactivespringexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSpringExampleApplication.class, args);
+	}
+
+}
